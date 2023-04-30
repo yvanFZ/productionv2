880df60793d8596b9c614fcb78c444cd0a4c56e7
yvanFZ <yvan@factoryzero.nl>
2023-04-30 21:51:23 +0200

    test 7

--- FZ/wsgi.py
+++ FZ/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FZ.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FZ.settings')
 # If so, then load the settings from production.py
-settings_module = 'FZ.production' if 'WEBSITE_HOSTNAME' in os.environ else 'FZ.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# settings_module = 'FZ.setting' if 'WEBSITE_HOSTNAME' in os.environ else 'FZ.settings'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
